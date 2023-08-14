db27967cf6bbc958f744c81e0a1194f1a99fa632
Ari-ell <ayethihan98@gmail.com>
2023-08-14 17:14:35 +0800

    Update FeedbackRepository.java

--- src/main/java/com/team1/backendApi/repository/FeedbackRepository.java
+++ src/main/java/com/team1/backendApi/repository/FeedbackRepository.java
@@ -1,9 +1,11 @@
 package com.team1.backendApi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.team1.backendApi.model.Feedback;
 
+@Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long>{
     
 }
